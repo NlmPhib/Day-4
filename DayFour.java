bd232fe7eda7606545cb7d3749432539e58de5f7
NlmPhib <129420106+NlmPhib@users.noreply.github.com>
2023-03-30 22:50:56 +0530

    Add files via upload
    Submitted

---
+++ DayFour.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class DayFour{
+    public static void main(String[] args){
+        int num1 = 10;
+        int num2 = 12;
+        int sum;
+        sum = num1+num2;
+            System.out.println("The sum of two integers is: "+sum);
+        return;
+        }
+    }
